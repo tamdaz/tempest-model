3fd4b55308d73b7c3225fd650c61364eda1902e9
Zohir Tamda <tamda.zohir.pro@gmail.com>
2026-02-28 19:56:38 +0100

    Fix the SQLite config. Delete the database.

--- config/database.config.php
+++ config/database.config.php
@@ -6,6 +6,10 @@ use function Tempest\env;
 use function Tempest\root_path;
 use Tempest\Database\Config\SQLiteConfig;
 
+/**
+ * Database configuration
+ * @see <URL>
+ */
 return new SQLiteConfig(
-    path: root_path(__DIR__ . '/' . env('DATABASE_PATH')),
+    path: root_path(env('DATABASE_PATH')),
 );
