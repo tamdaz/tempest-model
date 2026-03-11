ce2f8ddbdc4a0f8814a503b6436fb5f23cc05925
Zohir Tamda <tamda.zohir.pro@gmail.com>
2026-03-11 21:48:25 +0100

    Debug the test

--- tests/Controllers/PageControllerTest.php
+++ tests/Controllers/PageControllerTest.php
@@ -15,6 +15,6 @@ final class PageControllerTest extends IntegrationTestCase
     #[Test]
     public function homepage_is_reachable(): void
     {
-        $this->http->get('/')->assertOk()->dd();
+        $this->http->get('/')->dd();
     }
 }
